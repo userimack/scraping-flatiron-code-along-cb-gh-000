c1f0bf13be69e09219170e2259c7bfe07af8bd58
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-12 08:30:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,13 +18,6 @@ class Scraper
   
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    doc = self.get_page
-    doc.css(".post").each do |post|
-      course = Course.new 
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text 
-      course.description = post.css("p").text
-    end 
   end
   
   def get_courses
